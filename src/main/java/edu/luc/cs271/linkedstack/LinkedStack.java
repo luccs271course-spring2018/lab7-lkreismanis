878bbf56fc123259b1cdb644df1798c78e7ce20a
Larisa Kreismanis <lkreismanis@luc.edu>
2017-11-01 19:45:43 +0000

    Overall completion to LinkedStack and ReverseLines

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -1,44 +1,110 @@
 package edu.luc.cs271.linkedstack;
 
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class LinkedStack<E> implements IStack<E> {
 
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
 
-  // TODO why don't we need an explicit constructor?
+  public int size = 0;
+  // DONE why don't we need an explicit constructor?
+  // We do not need an explicit constructor because an empty default contructor will be made.
 
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
+    // DONE
+    top = new Node<>(obj, top);
+    size += 1;
+    return obj;
+    // return null;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      return top.data;
+    }
+    // return null;
   }
 
   @Override
   public E pop() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      E result = top.data;
+      top = top.next;
+      size -= 1;
+      return result;
+    }
+    // return null;
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
+    // DONE
+    if (top == null) {
+      return true;
+    }
     return false;
   }
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    return null;
+    // DONE implement using an ArrayList preallocated with the right size
+    // DONE add any instance variable(s) required to support this
+    final ArrayList<E> result = new ArrayList<>(size);
+    populateList(top, result); // TODO replace null with the right reference
+    return result;
+    // final ArrayList<E> list = new ArrayList<>(size);
+
+    // Node<E> curr = top;
+
+    // while (curr != null) {
+    //  list.add(curr.data);
+    // curr = curr.next;
+  }
+  // return list;
+  private void populateList(final Node<E> curr, final List<E> result) {
+    // DONE recursively populate the list in the desired order
+    if (curr != null) {
+      result.add(curr.data);
+    } else {
+      return;
+    }
+    populateList(curr.next, result);
+  }
+  /*
+  @Override
+  public List<E> asFifoList() {
+    final ArrayList<E> result = new ArrayList<>(size);
+    populateFifoList(top, result); // DONE replace null with the right reference
+    return result;
+  }
+  */
+  private void populateFifoList(final Node<E> curr, final List<E> result) {
+    // DONE recursively populate the list in the desired order
+    if (curr != null) {
+      result.add(0, curr.data);
+    } else {
+      return;
+    }
+    populateFifoList(curr.next, result);
+    /*
+      if (curr == null) {
+        return;
+      }
+      else {
+        result.add(0, curr.data);
+        populateFifoList(curr.next, result);
+      }
+    */
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -5,19 +5,35 @@ import java.util.Scanner;
 public class ReverseLines {
 
   public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
-
-
-
+    // DONE read successive input lines until EOF, then print out in reverse order
+    System.out.println("Print ReverseLines Executed");
     final Scanner input = new Scanner(System.in);
+    printReverse(input);
+  }
+
+  private static void printReverse(final Scanner input) {
+    String line;
+    line = input.nextLine();
+    LinkedStack<String> newStack = new LinkedStack<String>();
     while (input.hasNextLine()) {
-      final String line = input.nextLine();
-
-
-
+      line = input.nextLine();
+      newStack.push(line);
     }
-
-
-
+    while (!newStack.isEmpty()) {
+      System.out.println(newStack.pop());
+    }
+    // DONE recursively read and print successive input lines until EOF, then print them out in
+    // reverse order
+    if (input.hasNextLine()) {
+      // final String line = input.nextLine();
+      System.out.println(line);
+      printReverse(input);
+      System.out.println(line);
+    }
+    /*
+    else {
+      return;
+    }
+    */
   }
 }
